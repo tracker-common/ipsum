7f920365c736b268f708459bdae1151bf709e4a9
Marlena Compton <marlena.compton@gmail.com>
2013-08-10 14:13:55 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipsum::Application.config.secret_token = 'bbb28c9a98d67bcf7c68c7fe471e3a3e3a27c032b6d9fae047c3f9b93cd46c42918f423f269272d5bb5294621eca8e445a267cb4c2c331ed4ab9c119aa1bdabc'
